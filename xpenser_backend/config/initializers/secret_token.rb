dd01b3e8eeeb781f3045c72aa22fad922c2c9afe
sujithej <sujithej@gmail.com>
2013-06-29 00:54:04 +0530

    Backend Init

---
+++ xpenser_backend/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+XpenseTracker::Application.config.secret_token = '6306d84aed21637f4b8f1021b340ff644ff7ec952fb9c4c1478612067fd70a68d84672585ba2a173a9bd3370fb0b9a0ce3fbb6764b307e5e5fdfd4855963ba62'
