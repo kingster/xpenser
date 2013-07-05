d76fb680331b7e28517126c5f316d23588ac585d
sujithej <sujithej@gmail.com>
2013-07-05 21:38:30 +0530

    database setup

--- xpenser_backend/app/controllers/application_controller.rb
+++ xpenser_backend/app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
+  ##protect_from_forgery
+
+
+
+
+  get '/eval' do
+    "I got #{params[:command]}."
+  end
+
+
 end
--- xpenser_backend/app/controllers/users_controller.rb
+++ xpenser_backend/app/controllers/users_controller.rb
@@ -1,15 +1,41 @@
 require 'rubygems' # Only if installed via RubyGems
-
+require 'sinatra'
 
 class UsersController < ApplicationController
 
+  #def show
+  #
+  #  @data = "hi"
+  #  render :validate
+  #
+  #
+  #end
 
+  def index
+      puts "hello"
+
+  end
 
   def validate_user_name
 
-    @data =  params
+    begin
 
-    @msg = Time.now
+
+      client = Mysql2::Client.new(:host => "localhost", :username => "root" , :database => "expense_tracker")
+      results = client.query(  "select * from users" )
+      @data =  "We have #{results} row(s) \n <br>"
+      results.each do |row|
+        @data = "#{@data}#{row['username']}  \n <br> "
+      end
+
+      rescue Mysql2::Error => e
+        puts e
+      ensure
+        client.close if client
+
+      @msg = Time.now
+    end
+
   end
 
 
@@ -17,3 +43,15 @@ class UsersController < ApplicationController
 
 
 end
+
+
+
+
+
+
+
+
+
+
+
+
